6ea44290433280da12344695f7affecd98108972
ohganed <64299676+ohganed@users.noreply.github.com>
2026-08-11 06:16:27 +0900

    Publish English Structure Lab PWA

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE='english-structure-lab-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon-180.png','./app-icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
