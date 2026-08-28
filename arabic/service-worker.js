27eb8b225e26355d5b21263de8fe041e4e6f7217
ohganed <64299676+ohganed@users.noreply.github.com>
2026-08-28 19:40:33 +0900

    Bump Arabic cache after freeze fix

--- arabic/service-worker.js
+++ arabic/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='arabic-structure-lab-v0.4.1';
+const CACHE='arabic-structure-lab-v0.4.2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
