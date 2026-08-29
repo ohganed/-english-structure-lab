22b9f67da72fa666f3a02526f01bb9e7d9d8cf0c
ohganed <64299676+ohganed@users.noreply.github.com>
2026-08-30 05:34:37 +0900

    Bump Arabic cache after stabilization UI fix

--- arabic/service-worker.js
+++ arabic/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='arabic-structure-lab-v0.4.6';
+const CACHE='arabic-structure-lab-v0.4.7';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
