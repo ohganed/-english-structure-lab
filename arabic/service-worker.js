3cecdf6008de754bf816de07d7f9c508d66fd282
ohganed <64299676+ohganed@users.noreply.github.com>
2026-09-07 22:41:15 +0900

    Refresh lexicon pack 3 cache

--- arabic/service-worker.js
+++ arabic/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='arabic-structure-lab-v0.4.23';
+const CACHE='arabic-structure-lab-v0.4.24';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
